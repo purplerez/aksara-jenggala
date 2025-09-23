a3546e751771f44f2639630e6ca24e280e1a9184
purplerez <fairy.edensor@gmail.com>
2025-09-23 13:48:45 +0700

    fix : index

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-   <?php 
-        include "component/header.php";
-   ?>
-    <body data-bs-spy="scroll" data-bs-target="#header" tabindex="0">
-       <?php 
-            include "component/navbar.php";
+    <?php include "component/header.php"; ?>
+<body data-bs-spy="scroll" data-bs-target="#header" tabindex="0">
+    
 
-
-            // konten
-            $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-
-            if($page == 'home') include "page/home.php";
-            else if($page == 'shopall') include "page/shop-all.php";
-
-           
-            include "component/footer.php";
+        <!-- akan dirubah -->
+        <?php 
+            include "route/web.php";
         ?>
-        <script src="js/jquery-1.11.0.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-            crossorigin="anonymous"></script>
-        <script src="js/plugins.js"></script>
-        <script src="js/script.js"></script>
-    </body>
+        <?php //include "page/home.php"; ?>
+
+
+    <?php include "component/footer.php"; ?>
+
+	<script src="js/jquery-1.11.0.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+		crossorigin="anonymous"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/script.js"></script>
+
+</body>
+
 </html>
