f50e910c88b03d15dff5d6a6ce80722929c1479d
purplerez <fairy.edensor@gmail.com>
2025-09-26 08:36:20 +0700

    create : admin controller.

--- controller/AdminGenreController.php
+++ controller/AdminGenreController.php
@@ -11,15 +11,41 @@ class AdminGenreController {
     }
 
     public function index() {
-        include "page/admin/input_genre.php";
+
+        if(isset($_POST['submit'])) {
+            $this->genre->nama = $_POST['nama'];
+            if(isset($_GET['action']) && $_GET['action'] == 'edit' && isset($_GET['id'])) {
+                $this->genre->update($_GET['id']);
+            } else {
+                $this->genre->create();
+            }
+            header("Location: index.php?page=genre");
+            exit;
+        }
+        if(isset($_GET['action']) && $_GET['action'] == 'edit' && isset($_GET['id'])) {
+            $stmt = $this->genre->read_one($_GET['id']);
+            $result = $stmt->get_result();
+            $edit = $result->fetch_assoc();
+        }
+        include "page/input_genre.php";
     }
 
     public function read_all() {
         $stmt = $this->genre->read();
         $result = $stmt->get_result();
         $genres = $result->fetch_all(MYSQLI_ASSOC);
-        
-        include "page/admin/genre_all.php";
+
+        include "page/genre_all.php";
     }
 
+    public function delete($id){
+        $this->genre->delete($id);
+    }
+
+    public function edit($id) {
+        $stmt = $this->genre->read_one($id);
+        $result = $stmt->get_result();
+        $edit = $result->fetch_assoc();
+        // No need to include the form here, it's handled in index()
+    }
 }
