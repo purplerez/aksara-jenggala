9c5d3e4fc899497ac1239bf9b6f98410f9caaa71
purplerez <fairy.edensor@gmail.com>
2025-09-26 08:35:04 +0700

    add : input and delete modules for genre model

--- model/Genre.php
+++ model/Genre.php
@@ -4,7 +4,7 @@ class Genre{
     private $connection;
     private $table = "tb_genre";
 
-    public $id;
+    // public $id;
     public $nama;
 
     public function __construct($db){
@@ -18,6 +18,59 @@ class Genre{
         $stmt->execute();
 
         return $stmt;
+        }
+        catch(\Exception $e){
+            echo $e->getMessage();
+        }
+    }
+
+    public function read_one($id){
+        try{
+            $query = "SELECT * FROM ".$this->table." WHERE id = ?";
+            $stmt = $this->connection->prepare($query);
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            return $stmt;
+        }
+        catch(\Exception $e){
+            echo $e->getMessage();
+        }
+    }
+
+    public function create(){
+        try{
+            $query = "INSERT INTO ".$this->table." (nama) VALUES (?)";
+            $stmt = $this->connection->prepare($query);
+
+            $this->nama = htmlspecialchars(strip_tags($this->nama));
+            
+            $stmt->bind_param("s", $this->nama);
+            if ($stmt->execute())
+                return true;
+            return false;
+        }
+        catch (\Exception $e){
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function update(){
+        
+    }
+
+    public function delete($id)
+    {
+        try{
+            $query = "DELETE FROM ".$this->table." WHERE id = ?";
+            $stmt = $this->connection->prepare($query);
+            $stmt->bind_param("i", $id);
+            if ($stmt->execute())
+                return true;
+            else {
+                throw new \Exception($stmt->error);
+            }
+            return false;
         }
         catch(\Exception $e){
             echo $e->getMessage();
