e4a7cc8f271d1b830b4e9218607b54c7acff7fbf
purplerez <fairy.edensor@gmail.com>
2025-10-28 14:39:47 +0700

    add : admin dashboard

---
+++ page/backup-admin/admin-page/input_genre.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <form action="" method="post">
+        <input type="text" name="nama_genre" />
+        <textarea name="deskripsi" id="" cols="30" rows="10"></textarea>
+        <button type="submit">Simpan</button>
+    </form>
+</div>
--- route/admin.php
+++ route/admin.php
@@ -7,23 +7,26 @@ require_once('../../controller/controllers.php');
 
 
 $page = $_GET['page'] ?? 'dashboard';
-$action = $_GET['action'] ?? 'index';
-$genre = new AdminGenreController();
+
+// $action = $_GET['action'] ?? 'index';
+// $genre = new AdminGenreController();
 
 // Routes for admin panel
 
 switch ($page){
     case 'dashboard' : include "page/dashboard.php"; break;
-    case 'genre' : {  
-        switch ($action) {
-            case 'delete' : 
-                $genre->delete($_GET['id']); break;
-            case 'edit' : 
-                $genre->edit($_GET['id']);
-                break;
-        }  
-        $genre->index();
-        $genre->read_all();   
+    case 'genre' : include "admin-page/input_genre.php"; break;
+    
+    // case 'genre' : {  
+    //     switch ($action) {
+    //         case 'delete' : 
+    //             $genre->delete($_GET['id']); break;
+    //         case 'edit' : 
+    //             $genre->edit($_GET['id']);
+    //             break;
+    //     }  
+    //     $genre->index();
+    //     $genre->read_all();   
       
        
         // if($_POST) $genre->create();
